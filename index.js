e9012292308c89d349750a8abb3076ce8da0acc1
LITzman <31805301+ttnn5876@users.noreply.github.com>
2024-11-21 11:28:50 +0200

    Change seed

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const generateRandomUid = () => {
     return Math.random().toString(36).substring(2, 8);
 }
 
-SEED = 0.9878128464257857
+SEED = 12345
+
+const seededRandom = () => {
+    // Create a simple deterministic PRNG based on the seed
+    let value = SEED % 2147483647;
+    return function () {
+        value = (value * 16807) % 2147483647;
+        return (value - 1) / 2147483646;
+    };
+}
 
 const shuffleArray = (array) => {
+    const seed  = seededRandom()
     for (var i = array.length -1; i > 0; i--) {
 
-        var j = Math.floor(SEED * (i + 1))
+        var j = Math.floor(seed * (i + 1))
         var temp = array[i]
         array[i] = array[j]
         array[j] = temp
